5729d3f9045d72ea7a574db3548cc065438633f1
Theodore Kotz <ted@kotz.us>
2022-08-09 23:07:27 -0400

    done playing with malloc for today.

--- main.c
+++ main.c
@@ -1,8 +1,22 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <inttypes.h>
+
+#define MALLOC_WRAPPER malloc
+#define FREE_WRAPPER free
+
 
 int main( int argc, char** argv )
 {
-    printf("Hello\n");
+    int *abuf = malloc(4);
+    printf("Buf at 0x%" PRIXPTR "\n",(intptr_t)abuf);
+    //printf("Buf[x] = 0x%x\n",(int)abuf[5000000]);
+    free(abuf);
+    int *bbuf = malloc(4);
+    printf("Buf at 0x%" PRIXPTR "\n",(intptr_t)bbuf);
+    free(bbuf);
+    int *cbuf = malloc(4);
+    printf("Buf at 0x%" PRIXPTR "\n",(intptr_t)cbuf);
+    free(cbuf);
     return 0;
 }
